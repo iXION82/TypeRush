e3450e96c6103eeefd01f452e3a874b79dcf140b
Ayushman Singh Chauhan <895731a1@gmail.com>
2026-01-27 10:11:09 +0530

    Added a Login Page

--- frontend-project/src/App.tsx
+++ frontend-project/src/App.tsx
@@ -1,10 +1,12 @@
-import { Home } from './pages/home'
+// import { Home } from './pages/home'
+import LoginPage from './pages/login'
 import './App.css'
 function App() {
 
   return (
     <>
-      <Home/>
+    <LoginPage/>
+      {/* <Home/> */}
     </>
   )
 }
